b17a37c08f9be64a00330a15308b70c48c4e881b
fourlight <873039943@QQ.com>
2021-08-23 16:47:37 +0800

    Initial commit

---
+++ hyc/__init__.py
@@ -0,0 +1,2 @@
+version = '4.0.0b2'
+print('欢迎使用hyc库\n版本：%s\n更新日期：2021年8月21日\n'%version)
---
+++ hyc/percentage.py
@@ -0,0 +1,38 @@
+
+# 百分数类
+class Percentage:
+    def __init__(self, numerator):
+        self.numerator = numerator
+        self.one_unit = None
+
+    # 单位“1”加百分数
+    def __radd__(self, other):
+        self.one_unit = other
+        result = other + self.one_unit * self.numerator / 100
+        self.one_unit = None
+        return int(result)
+
+    # 单位“1”减百分数
+    def __rsub__(self, other):
+        self.one_unit = other
+        result = other - self.one_unit * (self.numerator / 100)
+        self.one_unit = None
+        return int(result)
+
+    # 单位“1”乘百分数
+    def __rmul__(self, other):
+        self.one_unit = other
+        result = other * (self.numerator / 100)
+        self.one_unit = None
+        return int(result)
+
+    # 单位“1”除以百分数
+    def __rtruediv__(self, other):
+        self.one_unit = other
+        result = other / (self.numerator / 100)
+        self.one_unit = None
+        return int(result)
+
+    # 百分数读作
+    def __repr__(self):
+        return f'{self.numerator}%'
